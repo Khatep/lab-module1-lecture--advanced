8e772897f792915153d17888924b504c5d1f582a
khatep <aserty664qer@gmail.com>
2026-01-07 13:22:06 +0500

    1. Сделал класс Student иммутабельным 2. Добавил валидацию данных для Order 3. Реализовал метод sendEmails

--- lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
+++ lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
@@ -4,4 +4,19 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 public record Order(long id, double price, LocalDateTime orderDate, Set<String> items ) {
+    public Order {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+
+        if (orderDate.isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Order date must be in the past");
+        }
+
+        if (items == null || items.isEmpty()) {
+            throw new IllegalArgumentException("Items must not be null or empty");
+        }
+
+        items = Set.copyOf(items);
+    }
 }
